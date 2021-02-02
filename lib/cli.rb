d8a9420101e20bcc111c9d526c9ee698a70eb783
nick <nicholas_scalione@yahoo.com>
2021-02-02 08:02:37 -0500

    added gems and gemfiles

---
+++ lib/cli.rb
@@ -0,0 +1,24 @@
+class Cli
+
+
+    def welcome
+        puts "Hello, Welcome to this app covering the NBA..."
+        self.prompt_for_selection
+    end
+
+
+    def prompt_for_selection
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
