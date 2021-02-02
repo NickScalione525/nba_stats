6ad97776bbc03f72f1c55055d8ca307d84a98fa7
nick <nicholas_scalione@yahoo.com>
2021-02-02 09:29:20 -0500

    revised my second method in Cli class, created first prompt

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+https://www.balldontlie.io/api/v1/teams
+
+https://www.balldontlie.io/api/v1/players
+
+https://www.balldontlie.io/api/v1/stats
+
--- lib/cli.rb
+++ lib/cli.rb
@@ -8,10 +8,33 @@ class Cli
 
 
     def prompt_for_selection
-        puts "Please enter a player's name"
+        puts "Please select a number"
+        puts "1. Teams"
+        puts "2. Players"
+        puts "3. Player Stats"
+        input = gets.strip
+        if input == "1"
+            self.teams
+        elsif input == "2"
+            self.players
+        elsif input == "3"
+            self.players_stats
+        else
+            puts "That is an invalid selection, please try again."
+        end
     end
 
+        def teams
+            
+        end
 
+        def players
+        
+        end
+
+        def players_stats
+           
+        end
 
 
 
